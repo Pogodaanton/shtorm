7aaf98e0b806073eca9afb14e97ffcfe1965edbd
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2019-04-17 21:21:16 +0200

    + DB-folder existence check

--- packages/nodemw-semi-server/lib/Controllers/databaseController.js
+++ packages/nodemw-semi-server/lib/Controllers/databaseController.js
@@ -1,8 +1,12 @@
+import fs from 'fs'
+import path from 'path'
 import low from 'lowdb'
 import FileSync from 'lowdb/adapters/FileSync'
 
+const dbFolder = path.join(__dirname, '../../db')
 class DatabaseController {
   constructor () {
+    if (!fs.existsSync(dbFolder)) fs.mkdirSync(dbFolder)
     this.adapter = new FileSync('db/nodemw-db.json')
     this.db = low(this.adapter)
     this.db.defaults({ configs: [], presets: [] }).write()

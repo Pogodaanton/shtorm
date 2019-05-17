aff54fae2fb8a99f98e1ca89e566d0458ecc6bda
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2019-05-17 19:49:53 +0200

    ~ nodemw-db -> shtorm-db

--- packages/shtorm-server/lib/Controllers/databaseController.js
+++ packages/shtorm-server/lib/Controllers/databaseController.js
@@ -7,7 +7,7 @@ const dbFolder = path.join(__dirname, '../../db')
 class DatabaseController {
   constructor () {
     if (!fs.existsSync(dbFolder)) fs.mkdirSync(dbFolder)
-    this.adapter = new FileSync('db/nodemw-db.json')
+    this.adapter = new FileSync('db/shtorm-db.json')
     this.db = low(this.adapter)
     this.db.defaults({ configs: [], presets: [] }).write()
   }

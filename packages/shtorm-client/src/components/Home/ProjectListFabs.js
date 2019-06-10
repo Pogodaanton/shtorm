0140cd1e9c6df65f2f4dca3f95fc258fe6b32e6c
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2019-06-10 15:38:41 +0200

    ~ Project add only when creatable

--- packages/shtorm-client/src/components/Home/ProjectListFabs.js
+++ packages/shtorm-client/src/components/Home/ProjectListFabs.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { Fab, Tooltip } from '@material-ui/core'
 import { Refresh, Add } from '@material-ui/icons'
 import { Link } from 'react-router-dom'
+import { UserContext } from '../../contexts/UserContext'
 
 const ProjectListFabs = ({ onUpdateListClick }) => (
   <div className='project-selector-fabs'>
@@ -14,15 +15,19 @@ const ProjectListFabs = ({ onUpdateListClick }) => (
         <Refresh />
       </Fab>
     </Tooltip>
-    <Tooltip title='Add project'>
-      <Fab
-        color='primary'
-        component={Link}
-        to='/projects/add'
-      >
-        <Add />
-      </Fab>
-    </Tooltip>
+    <UserContext.Consumer>
+      {({ getUserPermission }) => getUserPermission('createProjects') && (
+        <Tooltip title='Add project'>
+          <Fab
+            color='primary'
+            component={Link}
+            to='/projects/edit/add'
+          >
+            <Add />
+          </Fab>
+        </Tooltip>
+      )}
+    </UserContext.Consumer>
   </div>
 )
 
